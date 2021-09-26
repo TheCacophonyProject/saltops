433955e26bd8f795f8230fa23e66cf49cd4569b5
GP <g.ferraro22@gmail.com>
2021-09-27 10:01:02 +1300

    update to use str (#266)
    * update to use str
    
    * enforce str
    
    * ran black

--- _states/cacophony.py
+++ _states/cacophony.py
@@ -21,75 +21,74 @@ def pkg_installed_from_github(name, version, pkg_name=None, systemd_reload=True)
     if pkg_name == None:
         pkg_name = name
 
-    installed_version = __salt__['pkg.version'](pkg_name)
+    installed_version = __salt__["pkg.version"](pkg_name)
     if installed_version == version:
         return {
-            'name': pkg_name,
-            'result': True,
-            'comment': 'Version %s already installed.' % version,
-            'changes': {}
+            "name": pkg_name,
+            "result": True,
+            "comment": "Version %s already installed." % version,
+            "changes": {},
         }
 
-    source_url = 'https://github.com/TheCacophonyProject/{name}/releases/download/v{version}/{pkg_name}_{version}_arm.deb'.format(
+    source_url = "https://github.com/TheCacophonyProject/{name}/releases/download/v{version}/{pkg_name}_{version}_arm.deb".format(
         name=name,
         pkg_name=pkg_name,
         version=version,
     )
-    ret = __states__['pkg.installed'](
+    ret = __states__["pkg.installed"](
         name=name,
         version=version,
         sources=[{pkg_name: source_url}],
         refresh=False,
     )
 
-    if systemd_reload and ret['result'] and ret['changes'] and not __opts__['test']:
-        __salt__['cmd.run']('systemctl daemon-reload')
-        ret['comment'] += ' (systemd reloaded)'
+    if systemd_reload and ret["result"] and ret["changes"] and not __opts__["test"]:
+        __salt__["cmd.run"]("systemctl daemon-reload")
+        ret["comment"] += " (systemd reloaded)"
 
     return ret
 
 
 def init_alsa(name):
-    """Ensure that the built-in audio hardware is correctly initialised.
-    """
+    """Ensure that the built-in audio hardware is correctly initialised."""
     if _is_audio_setup():
         return {
-            'name': name,
-            'result': True,
-            'comment': "Audio already set up",
-            'changes': {}
+            "name": name,
+            "result": True,
+            "comment": "Audio already set up",
+            "changes": {},
         }
 
-    _remove_if_present('/var/lib/alsa/asound.state')
+    _remove_if_present("/var/lib/alsa/asound.state")
 
     # Play something (silence) to ensure the audio hardware is initialised
     # within ALSA.
     if not _play_silence():
         return {
-            'name': name,
-            'result': True,
-            'comment': "Playing audio failed (no audio hardware present?)",
-            'changes': {}
+            "name": name,
+            "result": True,
+            "comment": "Playing audio failed (no audio hardware present?)",
+            "changes": {},
         }
 
     # Save the ALSA state to disk.
-    subprocess.check_call(['alsactl', 'store'])
+    subprocess.check_call(["alsactl", "store"])
     return {
-        'name': name,
-        'result': True,
-        'comment': "ALSA state updated",
-        'changes': {
+        "name": name,
+        "result": True,
+        "comment": "ALSA state updated",
+        "changes": {
             name: {
-                'old': '',
-                'new': 'configured',
+                "old": "",
+                "new": "configured",
             },
         },
     }
 
 
 def _is_audio_setup():
-    output = subprocess.check_output("amixer")
-    return b"Simple mixer control 'PCM',0" in output
+    output = str(subprocess.check_output("amixer"))
+    return "Simple mixer control 'PCM',0" in output
 
 
 def _remove_if_present(name):
@@ -101,5 +100,7 @@ def _remove_if_present(name):
 
 def _play_silence():
     "Play 100ms of silence and return True if this succeeeded"
-    exit_code = subprocess.call("sox -n -t wav - trim 0.0 0.100 | play -q -", shell=True)
+    exit_code = subprocess.call(
+        "sox -n -t wav - trim 0.0 0.100 | play -q -", shell=True
+    )
     return exit_code == 0
